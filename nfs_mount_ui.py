a1f5c8ce29b7c09271ce0e5a287c542825481e18
iqlal <iqlal@tuta.io>
2024-01-09 14:40:55 +0700

    add sudo password window

--- nfs_mount_ui.py
+++ nfs_mount_ui.py
@@ -1,5 +1,31 @@
 import subprocess
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QDialog, QVBoxLayout, \
+    QHBoxLayout, QFormLayout, QDialogButtonBox
+
+class PasswordDialog(QDialog):
+    def __init__(self, parent=None):
+        super(PasswordDialog, self).__init__(parent)
+
+        self.setWindowTitle('Enter Sudo Password')
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.Password)
+
+        form_layout = QFormLayout()
+        form_layout.addRow('Password:', self.password_input)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+        layout.addLayout(form_layout)
+        layout.addWidget(button_box)
+
+        self.setLayout(layout)
+
+    def get_password(self):
+        return self.password_input.text()
+
 
 class NFSMountApp(QWidget):
     def __init__(self):
@@ -22,7 +48,7 @@ class NFSMountApp(QWidget):
         self.mount_point_input.setPlaceholderText('Enter Local Mount Point')
 
         self.mount_button = QPushButton('Mount', self)
-        self.mount_button.clicked.connect(self.mount)
+        self.mount_button.clicked.connect(self.show_password_dialog)
 
         layout.addWidget(self.server_label)
         layout.addWidget(self.server_input)
@@ -32,7 +58,15 @@ class NFSMountApp(QWidget):
 
         self.setLayout(layout)
 
-    def mount(self):
+    def show_password_dialog(self):
+        password_dialog = PasswordDialog(self)
+        result = password_dialog.exec_()
+
+        if result == QDialog.Accepted:
+            sudo_password = password_dialog.get_password()
+            self.mount(sudo_password)
+
+    def mount(self, sudo_password):
         server = self.server_input.text().strip()
         mount_point = self.mount_point_input.text().strip()
 
@@ -40,7 +74,7 @@ class NFSMountApp(QWidget):
             QMessageBox.warning(self, 'Input Error', 'Please enter both NFS Server and Mount Point.')
             return
 
-        command = f'sudo mount -t nfs {server}:/path/to/share {mount_point}'
+        command = f'echo {sudo_password} | sudo -S mount -t nfs {server}:/path/to/share {mount_point}'
 
         try:
             subprocess.run(command, shell=True, check=True)
